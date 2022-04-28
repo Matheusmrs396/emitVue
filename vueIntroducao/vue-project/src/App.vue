05123e75d5b981514c0029b40ce4096624edca02
MatheusSilva <matheusmrs396@gmail.com>
2022-04-28 09:56:58 -0300

    Comecando atividade emit

--- vueIntroducao/vue-project/src/App.vue
+++ vueIntroducao/vue-project/src/App.vue
@@ -1,70 +1,27 @@
-<script setup>
-
+<script>
+import TaskList from "./components/icons/TaskList.vue";
+export default {
+  components: { TaskList },
+  data() {
+    return {
+      toDo: [
+        { text: "teste1", id: 1 },
+        { text: "teste2", id: 2 },
+      ],
+      Done: [
+        { text: "teste3", id: 1 },
+        { text: "teste4", id: 2 },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-  <div></div>
+  <main>
+    <TaskList :tasks="toDo" />
+    <TaskList :tasks="Done" />
+  </main>
 </template>
 
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+<style></style>
---
+++ vueIntroducao/vue-project/src/components/icons/TaskList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: ["tasks"],
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in tasks" :key="item">
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
